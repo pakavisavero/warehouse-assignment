8c8baa8a26b220608a5a16cc862d5673a2c0a0a2
Savero Pakavi <revaztemellies@mbp15-spz.local>
2025-08-29 19:13:08 +0700

    package: Add connection to a database func

---
+++ server/config/init_db.go
@@ -0,0 +1,68 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/golang-migrate/migrate"
+	"github.com/golang-migrate/migrate/database/postgres"
+	"github.com/joho/godotenv"
+)
+
+func InitDB() (*sql.DB, error) {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	dbHost := GetEnv("DB_HOST", "localhost")
+	dbPort := GetEnv("DB_PORT", "5432")
+	dbUser := GetEnv("DB_USER", "postgres")
+	dbPassword := GetEnv("DB_PASSWORD", "password")
+	dbName := GetEnv("DB_NAME", "warehouse_db")
+
+	log.Printf("Connecting to database at %s:%s with user %s", dbHost, dbPort, dbUser)
+
+	connString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
+
+	db, err := sql.Open("postgres", connString)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	log.Println("Database connection established successfully.")
+	if err := applyMigrations(db); err != nil {
+		return nil, fmt.Errorf("failed to apply migrations: %w", err)
+	}
+
+	return db, nil
+}
+
+func applyMigrations(db *sql.DB) error {
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return fmt.Errorf("failed to create migration driver: %w", err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://migrations",
+		"postgres",
+		driver,
+	)
+
+	if err != nil {
+		return fmt.Errorf("failed to create migrate instance: %w", err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("failed to apply migrations: %w", err)
+	}
+
+	log.Println("Database migrations applied successfully.")
+	return nil
+}
